edfd62592b3c080023315eea3478d646ff56b08d
Yeonwoo Park <ywoo1234@gmail.com>
2019-02-09 18:42:24 -0500

    Add BuildControls component with styles

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import BuildControl from './BuildControl/BuildControl';
+
+import styles from './BuildControls.module.css';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+];
+
+const buildControls = props => (
+  <div className={styles.BuildControls}>
+    {controls.map(ctrl => {
+      return <BuildControl key={ctrl.label} label={ctrl.label} />;
+    })}
+  </div>
+);
+
+export default buildControls;
