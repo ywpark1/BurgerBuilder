85e482bc4ff62ed025a53bce33fd4d8f2304cd7d
Yeonwoo Park <ywoo1234@gmail.com>
2019-02-10 18:57:11 -0500

    Covnert Modal component to class component to enable rendering only when prop.show changes

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,48 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import styles from './Modal.module.css';
 
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-  <Fragment>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={styles.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-    >
-      {props.children}
-    </div>
-  </Fragment>
-);
+// const modal = props => (
+//   <Fragment>
+//     <Backdrop show={props.show} clicked={props.modalClosed} />
+//     <div
+//       className={styles.Modal}
+//       style={{
+//         transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+//         opacity: props.show ? '1' : '0'
+//       }}
+//     >
+//       {props.children}
+//     </div>
+//   </Fragment>
+// );
 
-export default modal;
+// export default modal;
+
+// Converted to class component to improve rendering performance
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} clicked={this.props.modalCextends} />
+        <div
+          className={styles.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Modal;
